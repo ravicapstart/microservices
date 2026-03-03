b2cc4bacccd7b93c5416b17be5b95058add4fb86
Ravi Khare <ravijobs1978@gmail.com>
2026-03-03 17:05:24 +0530

    celeray image resize

--- services/image-service/app/main.py
+++ services/image-service/app/main.py
@@ -1,18 +1,16 @@
 from fastapi import FastAPI
-from .tasks import resize_image, resize_image
+from .tasks import resize_image
 
 app = FastAPI()
 
 @app.post("/resize")
 def resize(payload : dict, width: int = 300, height: int = 300):
     
-    # payload = {
-    #     "image_id": 1,
-    #     "s3_key": "properties/original/test.jpg",
-    #     "width": 800,
-    #     "height": 600,
-    #     "callback_url": "http://your-django-ip:8000/api/update-image/"
-    # }
+#    {
+#     "image_id": 12,
+#     "s3_key": "properties/original/test.jpg",
+#     "callback_url": "http://django-ip/api/update-image/"
+#     }
     
     task = resize_image.delay(payload)
     return {"task_id": task.id}
--- services/image-service/app/tasks.py
+++ services/image-service/app/tasks.py
@@ -8,11 +8,7 @@ import os
 import boto3
 from dotenv import load_dotenv
 
-
-
 load_dotenv()
-
-
 
 celery = Celery(
     "worker",
@@ -26,34 +22,51 @@ s3 = boto3.client(
     region_name=os.getenv("AWS_REGION")
 )
 
+SIZES = {
+    "thumbnail": (200, 200),
+    "small": (400, 400),
+    "large": (800, 800),
+}
 
-def process_property_image(image_id):
-    image_obj = PropertyImage.objects.get(id=image_id)
+
+@celery.task(name="resize_image")
+def resize_image(payload: dict):
+
+    image_id = payload["image_id"]
+    s3_key = payload["s3_key"]
     
-    # Download image from S3
-    obj = s3.get_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key=image_obj.image.name)
-    img = Image.open(io.BytesIO(obj['Body'].read()))
-    
+    bucket = os.getenv("AWS_BUCKET_NAME")
+
+    obj = s3.get_object(Bucket=bucket, Key=s3_key)
+    img = Image.open(io.BytesIO(obj["Body"].read()))
+
     generated_urls = {}
 
-    # Resize and upload each size
     for size_name, size in SIZES.items():
         img_copy = img.copy()
         img_copy.thumbnail(size)
+
         buf = io.BytesIO()
         img_copy.save(buf, format="JPEG")
         buf.seek(0)
-        new_key =  f"properties/{size_name}/{image_obj.id}.jpg"
-        s3.put_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key=new_key, Body=buf, ContentType="image/jpeg")
-        
-          # ✅ Build URL
-        generated_urls[size_name] = f"https://{settings.AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/{new_key}"
 
-    # ✅ SAVE TO DATABASE
-    image_obj.thumbnail_url = generated_urls.get("thumbnail")
-    image_obj.small_url = generated_urls.get("small")
-    image_obj.large_url = generated_urls.get("large")
+        new_key = f"properties/{size_name}/{image_id}.jpg"
 
-    image_obj.save()
+        s3.put_object(
+            Bucket=bucket,
+            Key=new_key,
+            Body=buf,
+            ContentType="image/jpeg"
+        )
+
+        generated_urls[size_name] = f"https://{bucket}.s3.amazonaws.com/{new_key}"
+
+    # send callback to Django
+    requests.post(payload["callback_url"], json={
+        "image_id": image_id,
+        **generated_urls
+    })
+
+    return {"status": "done"}
     
     
